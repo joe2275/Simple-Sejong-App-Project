f39df0c9ca4dc239b6f929f2aeca6d710ad92f86
Jo Huijae <whgmlwo1000@gmail.com>
2018-11-07 13:17:09 +0900

    implementing Radio Button

---
+++ SejongApps/app/src/main/java/com/example/whgml/sejongapps/RadioButtonsActivity.java
@@ -0,0 +1,17 @@
+package com.example.whgml.sejongapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RadioButtonsActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radio_buttons);
+    }
+
+
+}
